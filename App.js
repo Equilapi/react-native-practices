c754dbd45aadf351c6ae841c70c0001c7175bb94
Elvis Quilapi <equilapi@meatcode.cl>
2025-11-08 19:28:41 -0300

    Add button to open web browser and include expo-web-browser dependency

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { openBrowserAsync } from 'expo-web-browser';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Click me" onPress={() => openBrowserAsync('https://www.google.com')} />
       <StatusBar style="auto" />
     </View>
   );
